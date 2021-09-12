9b91ae584f8fbf928ef956eb4fb69affa0a5b5c0
Sonali Singh <53177368+Sonali-Learntoshine@users.noreply.github.com>
2021-09-12 13:40:56 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'                      #Embedding pdf in webpage
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
